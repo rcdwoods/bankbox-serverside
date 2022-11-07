051b55fe2ad5ef2190c1f71bdcc038b31a5c21f5
Richard Nascimento <richard.nascimento@ilia.digital>
2022-11-07 19:59:31 -0300

    :tada: Init project

---
+++ src/main/java/com/bankbox/BankboxApplication.java
@@ -0,0 +1,13 @@
+package com.bankbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankbox/BankboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
