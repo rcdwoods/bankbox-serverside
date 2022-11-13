774b6612ac7e6b5f8faa86f7c56ef7798b78fb24
C1329677 Richard Pereira do Nascimento <C1329677@interno.bb.com.br>
2022-11-12 23:32:18 -0300

    :hammer: Create Costumer DTO

---
+++ src/main/java/com/bankbox/dto/CostumerDTO.java
@@ -0,0 +1,11 @@
+package com.bankbox.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class CostumerDTO {
+	public Long id;
+	@NotBlank
+	public String name;
+	@NotBlank
+	public String cpf;
+}
