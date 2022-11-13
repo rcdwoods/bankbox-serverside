7f4d0ed7d244b72d55bc5fecf50d9defc2ca4763
C1329677 Richard Pereira do Nascimento <C1329677@interno.bb.com.br>
2022-11-12 23:35:50 -0300

    :hammer: Create Costumer resource

---
+++ src/main/java/com/bankbox/resource/CostumerResource.java
@@ -0,0 +1,48 @@
+package com.bankbox.resource;
+
+import com.bankbox.converter.CostumerConverter;
+import com.bankbox.domain.Costumer;
+import com.bankbox.dto.CostumerDTO;
+import com.bankbox.service.costumer.impl.CostumerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/costumers")
+public class CostumerResource {
+
+	private final CostumerService costumerService;
+	private final CostumerConverter costumerConverter;
+
+	public CostumerResource(CostumerService costumerService, CostumerConverter costumerConverter) {
+		this.costumerService = costumerService;
+		this.costumerConverter = costumerConverter;
+	}
+
+	@PostMapping
+	public ResponseEntity<CostumerDTO> createCostumer(@Valid  @RequestBody CostumerDTO costumerDto) {
+		Costumer costumer = costumerConverter.toCostumer(costumerDto);
+		Costumer costumerCreated = costumerService.createCostumer(costumer);
+		return ResponseEntity.ok(costumerConverter.toDto(costumerCreated));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CostumerDTO>> retrieveCostumers() {
+		List<Costumer> costumersFound = costumerService.retrieveAll();
+		return ResponseEntity.ok(costumerConverter.toDto(costumersFound));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<CostumerDTO> retrieveCostumer(@RequestParam Long id) {
+		Costumer costumerFound = costumerService.retrieveById(id);
+		return ResponseEntity.ok(costumerConverter.toDto(costumerFound));
+	}
+}
---
+++ src/main/java/com/bankbox/resource/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.bankbox.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException exception) {
+			Map<String, String> errors = new HashMap<>();
+			exception.getBindingResult().getAllErrors().forEach(error -> {
+				String fieldName = ((FieldError) error).getField();
+				String errorMessage = error.getDefaultMessage();
+				errors.put(fieldName, errorMessage);
+			});
+			return errors;
+	}
+}
