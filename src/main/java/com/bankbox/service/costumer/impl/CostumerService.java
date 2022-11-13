62c5abaaae5fe6dc5eacba4dcbe474cf0fa731e3
C1329677 Richard Pereira do Nascimento <C1329677@interno.bb.com.br>
2022-11-12 23:31:46 -0300

    :hammer: Create Costumer service implementing two interfaces

--- src/main/java/com/bankbox/service/costumer/impl/CostumerService.java
+++ src/main/java/com/bankbox/service/costumer/impl/CostumerService.java
@@ -1,11 +1,14 @@
-package com.bankbox.service.impl;
+package com.bankbox.service.costumer.impl;
 
+import com.bankbox.constant.ExceptionMessage;
 import com.bankbox.domain.Costumer;
+import com.bankbox.exception.CostumerNotFoundException;
 import com.bankbox.repository.CostumerRepository;
-import com.bankbox.service.CreateCostumer;
-import com.bankbox.service.RetrieveCostumer;
+import com.bankbox.service.costumer.CreateCostumer;
+import com.bankbox.service.costumer.RetrieveCostumer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,8 +21,15 @@ public class CostumerService implements RetrieveCostumer, CreateCostumer {
 	}
 
 	@Override
-	public Optional<Costumer> retrieveCostumer() {
-		return Optional.empty();
+	public List<Costumer> retrieveAll() {
+		return costumerRepository.findAll();
+	}
+
+	@Override
+	public Costumer retrieveById(Long id) {
+		Optional<Costumer> costumerFound = costumerRepository.findById(id);
+		if (costumerFound.isEmpty()) throw new CostumerNotFoundException(ExceptionMessage.COSTUMER_NOT_FOUND);
+		return costumerFound.get();
 	}
 
 	@Override
