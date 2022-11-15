13d8f4afb202a5118eac5ff5c25d3557fb1ff9cc
Richard Nascimento <rcdwoods@gmail.com>
2022-11-15 20:05:46 -0300

    :hammer: Create interface that will provide bank account creation

---
+++ src/main/java/com/bankbox/service/bankaccount/CreateBankAccount.java
@@ -0,0 +1,7 @@
+package com.bankbox.service.bankaccount;
+
+import com.bankbox.domain.BankAccount;
+
+public interface CreateBankAccount {
+	public BankAccount createBankAccount(BankAccount bankAccount);
+}
