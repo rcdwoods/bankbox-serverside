df76e78b71ab9ff55d7539116eee0223665d8546
Richard Nascimento <rcdwoods@gmail.com>
2022-11-18 22:05:07 -0300

    :hammer: Add retriving methods to BankAccount Service

--- src/main/java/com/bankbox/service/bankaccount/impl/BankAccountService.java
+++ src/main/java/com/bankbox/service/bankaccount/impl/BankAccountService.java
@@ -3,10 +3,14 @@ package com.bankbox.service.bankaccount.impl;
 import com.bankbox.domain.BankAccount;
 import com.bankbox.repository.BankAccountRepository;
 import com.bankbox.service.bankaccount.CreateBankAccount;
+import com.bankbox.service.bankaccount.RetrieveBankAccount;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class BankAccountService implements CreateBankAccount {
+public class BankAccountService implements CreateBankAccount, RetrieveBankAccount {
 
 	private final BankAccountRepository bankAccountRepository;
 
@@ -17,5 +21,17 @@ public class BankAccountService implements CreateBankAccount {
 	@Override
 	public BankAccount createBankAccount(BankAccount bankAccount) {
 		return bankAccountRepository.save(bankAccount);
+	}
+
+	@Override
+	public List<BankAccount> retrieveByUser(Long id) {
+		return bankAccountRepository.findByOwnerId(id);
+	}
+
+	@Override
+	public BankAccount retrieveByAgencyAndAccount(String agency, String account) {
+		Optional<BankAccount> bankAccountFound = bankAccountRepository.findByAgencyAndAccount(agency, account);
+		if (bankAccountFound.isEmpty()) throw new RuntimeException("Bank account not found");
+		return bankAccountFound.get();
 	}
 }
