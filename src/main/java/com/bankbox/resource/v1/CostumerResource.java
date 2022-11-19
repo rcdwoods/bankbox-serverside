99954b2a1b833ecbd0cdf71a9f3522bcbd93735d
Richard Nascimento <rcdwoods@gmail.com>
2022-11-19 16:34:26 -0300

    :hammer: Create endpoint to get costumer balance detailed

--- src/main/java/com/bankbox/resource/v1/CostumerResource.java
+++ src/main/java/com/bankbox/resource/v1/CostumerResource.java
@@ -2,10 +2,12 @@ package com.bankbox.resource.v1;
 
 import com.bankbox.converter.CostumerConverter;
 import com.bankbox.domain.Costumer;
+import com.bankbox.dto.BalanceDetailsResponse;
 import com.bankbox.dto.CostumerBasicDTO;
 import com.bankbox.dto.CostumerDTO;
 import com.bankbox.dto.CostumerRegisterDTO;
-import com.bankbox.service.costumer.impl.CostumerService;
+import com.bankbox.service.costumer.CreateCostumer;
+import com.bankbox.service.costumer.RetrieveCostumer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,37 +18,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/costumers")
 @CrossOrigin(origins = "*")
 public class CostumerResource {
 
-	private final CostumerService costumerService;
+	private final RetrieveCostumer retrieveCostumer;
+	private final CreateCostumer createCostumer;
 	private final CostumerConverter costumerConverter;
 
-	public CostumerResource(CostumerService costumerService, CostumerConverter costumerConverter) {
-		this.costumerService = costumerService;
+	public CostumerResource(
+		RetrieveCostumer retrieveCostumer,
+		CreateCostumer createCostumer,
+		CostumerConverter costumerConverter
+	) {
+		this.retrieveCostumer = retrieveCostumer;
+		this.createCostumer = createCostumer;
 		this.costumerConverter = costumerConverter;
 	}
 
 	@PostMapping
 	public ResponseEntity<CostumerDTO> createCostumer(@Valid  @RequestBody CostumerRegisterDTO costumerRegisterDto) {
 		Costumer costumer = costumerConverter.toCostumer(costumerRegisterDto);
-		Costumer costumerCreated = costumerService.createCostumer(costumer);
+		Costumer costumerCreated = createCostumer.createCostumer(costumer);
 		return ResponseEntity.ok(costumerConverter.toDTO(costumerCreated));
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<CostumerDTO> retrieveCostumer(@PathVariable Long id) {
-		Costumer costumerFound = costumerService.retrieveById(id);
+		Costumer costumerFound = retrieveCostumer.retrieveById(id);
 		return ResponseEntity.ok(costumerConverter.toDTO(costumerFound));
 	}
 
 	@GetMapping("/{cpf}/basic")
 	public ResponseEntity<CostumerBasicDTO> retrieveCostumerBasic(@PathVariable String cpf) {
-		Costumer costumerFound = costumerService.retrieveByCpf(cpf);
+		Costumer costumerFound = retrieveCostumer.retrieveByCpf(cpf);
 		return ResponseEntity.ok(costumerConverter.toBasic(costumerFound));
+	}
+
+	@GetMapping("/id/balance")
+	public ResponseEntity<BalanceDetailsResponse> retrieveCostumerBalanceDetails(@PathVariable Long id) {
+		Costumer costumerFound = retrieveCostumer.retrieveById(id);
+		return ResponseEntity.ok(costumerConverter.toBalanceDetails(costumerFound));
 	}
 }
