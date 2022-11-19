247a315a95b6b3e9909117fa0c778b3763b9add9
Richard Nascimento <rcdwoods@gmail.com>
2022-11-18 22:04:47 -0300

    :hammer: Add methods to BankAccount Repository

--- src/main/java/com/bankbox/repository/BankAccountRepository.java
+++ src/main/java/com/bankbox/repository/BankAccountRepository.java
@@ -3,5 +3,11 @@ package com.bankbox.repository;
 import com.bankbox.domain.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+	List<BankAccount> findByOwnerId(Long id);
+	Optional<BankAccount> findByAgencyAndAccount(String agency, String account);
 }
