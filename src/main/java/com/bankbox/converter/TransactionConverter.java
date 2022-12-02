0c34a46d4297fdb07e30d43b9d50c9e773e683a4
Richard Nascimento <rcdwoods@gmail.com>
2022-12-02 17:34:48 -0300

    :hammer: Add method to TransactionConverter responsible for transforming Transactions from BOTH flow into two ones of type inbound and outbound

--- src/main/java/com/bankbox/converter/TransactionConverter.java
+++ src/main/java/com/bankbox/converter/TransactionConverter.java
@@ -3,15 +3,14 @@ package com.bankbox.converter;
 import com.bankbox.domain.BankAccount;
 import com.bankbox.domain.Transaction;
 import com.bankbox.domain.TransactionType;
-import com.bankbox.dto.BankAccountBasicResponse;
 import com.bankbox.dto.DateTransactionsResponse;
+import com.bankbox.dto.TransactionFlow;
 import com.bankbox.dto.TransactionRequest;
 import com.bankbox.dto.TransactionResponse;
 import com.bankbox.service.bankaccount.RetrieveBankAccount;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,23 +21,14 @@ import java.util.stream.Collectors;
 @Component
 public class TransactionConverter {
 	private final RetrieveBankAccount retrieveBankAccount;
-	private final BankConverter bankConverter;
 	private final BankAccountConverter bankAccountConverter;
 
 	public TransactionConverter(
 		RetrieveBankAccount retrieveBankAccount,
-		BankConverter bankConverter,
 		BankAccountConverter bankAccountConverter
 	) {
 		this.retrieveBankAccount = retrieveBankAccount;
-		this.bankConverter = bankConverter;
 		this.bankAccountConverter = bankAccountConverter;
-	}
-
-	public List<Transaction> toModel(List<TransactionRequest> requests) {
-		return requests.stream()
-			.map(this::toModel)
-			.collect(Collectors.toList());
 	}
 
 	public Transaction toModel(TransactionRequest request) {
@@ -47,13 +37,14 @@ public class TransactionConverter {
 		return new Transaction(source, beneficiary, TransactionType.valueOf(request.type), request.value);
 	}
 
-	public List<TransactionResponse> toResponse(List<Transaction> transactions) {
-		return transactions.stream()
-			.map(this::toResponse)
+	public List<TransactionResponse> toResponse(List<Transaction> transactions, Long customerId) {
+		List<TransactionResponse> transactionsResponse = transactions.stream()
+			.map(transaction -> toResponse(transaction, customerId))
 			.collect(Collectors.toList());
+		return transformFlowBothToInboundAndOutbound(transactionsResponse);
 	}
 
-	public TransactionResponse toResponse(Transaction transaction) {
+	public TransactionResponse toResponse(Transaction transaction, Long customerId) {
 		TransactionResponse response = new TransactionResponse();
 		response.id = transaction.getId();
 		response.sourceId = transaction.getSource().getId();
@@ -63,15 +54,30 @@ public class TransactionConverter {
 		response.type = transaction.getType();
 		response.value = transaction.getValue();
 		response.performedAt = transaction.getPerformedAt();
+		response.flow = transaction.flowForCostumer(customerId);
 		return response;
 	}
 
-	public List<DateTransactionsResponse> toDateTransactionResponse(List<Transaction> transactions) {
-		List<TransactionResponse> transactionsConverted = toResponse(transactions);
+	public List<DateTransactionsResponse> toDateTransactionResponse(List<Transaction> transactions, Long customerId) {
+		List<TransactionResponse> transactionsConverted = toResponse(transactions, customerId);
 		Map<LocalDate, List<TransactionResponse>> map = mapTransactionsByDate(transactionsConverted);
 		List<DateTransactionsResponse> dateTransactions = new ArrayList<>();
 		map.forEach((performedAt, transactionsResponse) -> dateTransactions.add(new DateTransactionsResponse(performedAt, transactionsResponse)));
 		return dateTransactions;
+	}
+
+	private List<TransactionResponse> transformFlowBothToInboundAndOutbound(final List<TransactionResponse> transactions) {
+		List<TransactionResponse> inboundTransactions = transactions.stream()
+			.filter(it -> it.flow == TransactionFlow.BOTH)
+			.peek(it -> it.flow = TransactionFlow.INBOUND)
+			.collect(Collectors.toList());
+		List<TransactionResponse> outboundTransactions = inboundTransactions.stream()
+			.map(TransactionResponse::new)
+			.peek(transaction -> transaction.flow = TransactionFlow.OUTBOUND)
+			.collect(Collectors.toList());
+
+		transactions.addAll(outboundTransactions);
+		return transactions;
 	}
 
 	private Map<LocalDate, List<TransactionResponse>> mapTransactionsByDate(List<TransactionResponse> transactions) {
