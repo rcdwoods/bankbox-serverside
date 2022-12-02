ad71d88030d55ece4be429c77093d10a687b8d1b
Richard Nascimento <rcdwoods@gmail.com>
2022-12-02 17:30:58 -0300

    :hammer: Add tests to new method in Transaction class

--- src/test/java/com/bankbox/domain/TransactionTest.java
+++ src/test/java/com/bankbox/domain/TransactionTest.java
@@ -1,6 +1,7 @@
 package com.bankbox.domain;
 
 import com.bankbox.constant.ExceptionMessage;
+import com.bankbox.dto.TransactionFlow;
 import com.bankbox.exception.BalanceNotEnoughException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -41,6 +42,54 @@ class TransactionTest {
 
 		Assertions.assertThat(exception.getMessage()).isEqualTo(ExceptionMessage.BALANCE_NOT_ENOUGH);
 	}
+
+	@Test
+	void mustReturnInboundFlowWhenCustomerIsBeneficiary() {
+		Costumer source = new Costumer();
+		source.setId(1L);
+		BankAccount sourceAccount = factoryBankAccount(source, 845.18);
+		source.addBankAccount(sourceAccount);
+		Costumer beneficiary = new Costumer();
+		beneficiary.setId(2L);
+		BankAccount beneficiaryAccount = factoryBankAccount(beneficiary, 99.23);
+		beneficiary.addBankAccount(beneficiaryAccount);
+
+		Transaction transaction = new Transaction(sourceAccount, beneficiaryAccount, TransactionType.PIX, BigDecimal.valueOf(242.13));
+
+		Assertions.assertThat(transaction.flowForCostumer(2L)).isEqualTo(TransactionFlow.INBOUND);
+	}
+
+	@Test
+	void mustReturnOutboundFlowWhenCustomerIsSource() {
+		Costumer source = new Costumer();
+		source.setId(1L);
+		BankAccount sourceAccount = factoryBankAccount(source, 845.18);
+		source.addBankAccount(sourceAccount);
+		Costumer beneficiary = new Costumer();
+		beneficiary.setId(2L);
+		BankAccount beneficiaryAccount = factoryBankAccount(beneficiary, 99.23);
+		beneficiary.addBankAccount(beneficiaryAccount);
+
+		Transaction transaction = new Transaction(sourceAccount, beneficiaryAccount, TransactionType.PIX, BigDecimal.valueOf(242.13));
+
+		Assertions.assertThat(transaction.flowForCostumer(1L)).isEqualTo(TransactionFlow.OUTBOUND);
+	}
+
+	@Test
+	void mustThrowExceptionWhenCustomerIsNotInvoledInTransaction() {
+		Costumer source = new Costumer();
+		source.setId(1L);
+		BankAccount sourceAccount = factoryBankAccount(source, 845.18);
+		source.addBankAccount(sourceAccount);
+		Costumer beneficiary = new Costumer();
+		beneficiary.setId(2L);
+		BankAccount beneficiaryAccount = factoryBankAccount(beneficiary, 99.23);
+		beneficiary.addBankAccount(beneficiaryAccount);
+
+		Transaction transaction = new Transaction(sourceAccount, beneficiaryAccount, TransactionType.PIX, BigDecimal.valueOf(242.13));
+
+		assertThrows(IllegalArgumentException.class, () -> transaction.flowForCostumer(3L));
+}
 
 	private BankAccount factoryBankAccount(Costumer costumer, Double balance) {
 		return new BankAccount(costumer, BankName.ITAU, BankAccountType.SAVINGS, BigDecimal.valueOf(balance), "1322", "1233-2");
