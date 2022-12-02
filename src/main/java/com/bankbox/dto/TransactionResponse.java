4d0a010ca8b44c05df6681ed42f4e8cfd7356d16
Richard Nascimento <rcdwoods@gmail.com>
2022-12-02 17:35:41 -0300

    :hammer: Add a method to clone TransactionResponse object

--- src/main/java/com/bankbox/dto/TransactionResponse.java
+++ src/main/java/com/bankbox/dto/TransactionResponse.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public class TransactionResponse {
+public class TransactionResponse implements Cloneable {
 
 	public Long id;
 	@JsonProperty("source_id")
@@ -19,4 +19,19 @@ public class TransactionResponse {
 	public BigDecimal value;
 	@JsonProperty("performed_at")
 	public LocalDateTime performedAt;
+	public TransactionFlow flow;
+
+	public TransactionResponse() { }
+
+	public TransactionResponse(TransactionResponse transactionResponse) {
+		this.id = transactionResponse.id;
+		this.sourceId = transactionResponse.sourceId;
+		this.beneficiaryId = transactionResponse.beneficiaryId;;
+		this.source = transactionResponse.source;
+		this.beneficiary = transactionResponse.beneficiary;
+		this.type = transactionResponse.type;
+		this.value = transactionResponse.value;
+		this.performedAt = transactionResponse.performedAt;
+		this.flow = transactionResponse.flow;
+	}
 }
