52c564d64b4cacdc3aa0aaa38b0a4681606aa5b7
Richard Nascimento <rcdwoods@gmail.com>
2022-12-03 19:57:06 -0300

    :hammer: Add DISTINCT clasule to avoid duplicated value

--- src/main/java/com/bankbox/repository/TransactionRepository.java
+++ src/main/java/com/bankbox/repository/TransactionRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-	@Query(value = "SELECT t, t FROM Transaction t JOIN BankAccount b on b.id = t.source.id or b.id = t.beneficiary.id WHERE b.owner.id = :id")
+	@Query(value = "SELECT DISTINCT t FROM Transaction t JOIN BankAccount b on b.id = t.source.id or b.id = t.beneficiary.id WHERE b.owner.id = :id")
 	List<Transaction> findBySourceOrBeneficiary(Long id);
 }
