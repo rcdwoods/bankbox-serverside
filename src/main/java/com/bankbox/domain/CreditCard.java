2d4b1cac9a6ee7c709784f905800b5cdeba46c14
Richard Nascimento <rcdwoods@gmail.com>
2022-12-04 14:27:33 -0300

    :hammer: Create entity to represent CrediCards

---
+++ src/main/java/com/bankbox/domain/CreditCard.java
@@ -0,0 +1,77 @@
+package com.bankbox.domain;
+
+import com.bankbox.constant.Constant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import java.time.YearMonth;
+
+@Entity
+public class CreditCard {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	private String owner;
+	@ManyToOne
+	private Costumer customer;
+	@NotNull
+	private String number;
+	@NotNull
+	private YearMonth expiration;
+	@NotNull
+	private int securityNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		String formattedNumber = number.replace(" ", "");
+		if (formattedNumber.length() != Constant.CREDIT_CARD_LENGTH)
+			throw new IllegalArgumentException("Incorrect credit card length");
+		this.number = formattedNumber;
+	}
+
+	public String getLastNumbers() {
+		return number.subSequence(12, 16).toString();
+	}
+
+	public YearMonth getExpiration() {
+		return expiration;
+	}
+
+	public void setExpiration(YearMonth expiration) {
+		this.expiration = expiration;
+	}
+
+	public int getSecurityNumber() {
+		return securityNumber;
+	}
+
+	public void setSecurityNumber(int securityNumber) {
+		if (String.valueOf(securityNumber).length() != Constant.SECURITY_NUMBER_LENGTH)
+			throw new IllegalArgumentException("Incorrect credit card security number");
+		this.securityNumber = securityNumber;
+	}
+}
