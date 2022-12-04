1f425b217f34e2339810d45864e03c02e434c849
Richard Nascimento <rcdwoods@gmail.com>
2022-12-04 14:27:10 -0300

    :hammer: Create Constant class with a private constructor to keep useful constants

---
+++ src/main/java/com/bankbox/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.bankbox.constant;
+
+public class Constant {
+	private Constant() {}
+
+	public static final int CREDIT_CARD_LENGTH = 16;
+	public static final int SECURITY_NUMBER_LENGTH = 3;
+}
